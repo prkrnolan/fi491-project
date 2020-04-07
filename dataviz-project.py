949208004b247fe829a072dd3c3af92c00ac965b
sirsigmund79 <parker.k.nolan@gmail.com>
2020-04-07 13:08:03 -0400

    Added charts of loan stats by grade and purpose

--- dataviz-project.py
+++ dataviz-project.py
@@ -16,12 +16,18 @@ loans_old = pd.read_csv('loan.csv', header=0, low_memory = False, nrows=400000)
 loans_new = pd.read_csv('lendingclub-individual.csv', header=0, low_memory = False)
 
 lcd = pd.concat([loans_new, loans_old])
-
+#
 lcd.groupby(['grade', 'issue_d']).mean()['funded_amnt'].unstack().plot(kind='bar',figsize=(20, 12), title= 'Average loan amount by credit grade and date')
 
 lcd.groupby(['grade', 'issue_d']).count()['funded_amnt'].unstack().plot(kind='bar',figsize=(20, 12), title = 'Count of loans issued by credit grade and date')
 
 lcd.groupby(['grade', 'issue_d']).median()['funded_amnt'].unstack().plot(kind='bar',figsize=(20, 12), title= 'Median loan amount by credit grade and date')
+
+lcd.groupby(['grade', 'purpose']).median()['funded_amnt'].unstack().plot(kind='bar',figsize=(20, 12), title= 'Median loan amount by credit grade and application type')
+
+lcd.groupby(['grade', 'purpose']).count()['funded_amnt'].unstack().plot(kind='bar',figsize=(20, 12), title= 'Count of loans by credit grade and application type')
+
+lcd.groupby(['grade', 'purpose']).mean()['funded_amnt'].unstack().plot(kind='bar',figsize=(20, 12), title= 'Average loan amount by credit grade and application type')
 
 #lcd['int_rate'].plot(figsize=(20, 10), lw=.1)
 #ask = lcd['loan_amnt']
